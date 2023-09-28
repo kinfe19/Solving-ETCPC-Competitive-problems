86c10f737964a66ca80cad0c98e79023f5c9cb58
kinfe19 <81519060+kinfe19@users.noreply.github.com>
2023-09-28 07:42:29 +0300

    Create gcdandlcm

---
+++ gcdandlcm.py
@@ -0,0 +1,28 @@
+#  implementation of gcd and lcm
+# Author: Kinfe Masrie
+def gcd(a, b):
+    """
+    Euclidean algorithm implementation
+    """
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """
+    Least common multiple implementation
+    """
+    return a * b // gcd(a, b)
+
+
+def example():
+    """
+    Example
+    """
+    print(gcd(12, 8)) # 4
+    print(lcm(12, 8)) # 24
+
+
+if __name__ == "__main__":
+    example()
